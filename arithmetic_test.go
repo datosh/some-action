43b72a2ad8e164893304a6c51e49f3ec3275f669
Fabian Kammel <fk@edgeless.systems>
2022-10-12 13:43:34 +0000

    add a go dependency
    Signed-off-by: Fabian Kammel <fk@edgeless.systems>

--- arithmetic_test.go
+++ arithmetic_test.go
@@ -1,6 +1,10 @@
 package someaction
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestAdd(t *testing.T) {
 	type args struct {
@@ -19,7 +23,7 @@ func TestAdd(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Add(tt.args.a, tt.args.b); got != tt.want {
-				t.Errorf("Add() = %v, want %v", got, tt.want)
+				assert.Equal(t, tt.want, got)
 			}
 		})
 	}
