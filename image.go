eecc0cb4a8ea0bd30c84b46044688e056960a361
Fabian Kammel <fk@edgeless.systems>
2022-11-23 15:23:12 +0000

    updates & cleanup
    Signed-off-by: Fabian Kammel <fk@edgeless.systems>

--- image.go
+++
@@ -1,5 +0,0 @@
-package someaction
-
-func Image() string {
-	return "ghcr.io/edgelesssys/cloud-provider-gcp:v23.0.0@sha256:bf54ecb58fef5b1358d1dd25b1068598a74adbc7e7622b42a2708d1ed4bdc4bc" // renovate:container
-}
